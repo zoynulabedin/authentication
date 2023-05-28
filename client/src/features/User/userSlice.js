11e4455b00f2604a90614ae58222dfa184204d93
Zoynul <33788041+zoynulabedin@users.noreply.github.com>
2023-05-28 12:18:56 +0600

    redux-toolkit

--- client/src/features/User/userSlice.js
+++ client/src/features/User/userSlice.js
@@ -1,4 +1,19 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const userSlice = createSlice();
+export const userSlice = createSlice({
+	name: "user",
+	initialState: {
+		users: [],
+		loading: false,
+		message: null,
+		error: null,
+	},
+	reducers: {},
+	extraReducers: (builder) => {},
+});
 
+// export slice selectors
+
+// slice exports
+export const {} = userSlice.actions;
+export default userSlice.reducer;
